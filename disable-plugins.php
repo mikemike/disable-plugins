a4bf72bb6d3afb0400eb140876891956d8a3338d
Mike Griffiths <mike@recklessnewmedia.com>
2014-08-14 17:11:41 +0100

    Initial commit, scaffold structure in place

---
+++ disable-plugins.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Disable All Active Plugins
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: disable-plugins
+Domain Path: /languages
+*/
+
